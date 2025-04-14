cc20ae43e29e6a1d2ae3a6d74e8cda49e69e0ea9
Om-kamboj <om0652.becse24@chitkara.edu.in>
2025-04-14 17:34:04 +0530

    added link for contact

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -12,11 +12,14 @@ export default function Footer() {
           &copy; {new Date().getFullYear()} Pokeinfo.All rights reserved.
         </p>
         <div className="flex justify-center space-x-4 mt-2">
-        <a href="https://github.com/yashdeepsingh2006" className="hover:text-gray-400">Yashdeep Singh</a>
+          <a href="https://github.com/yashdeepsingh2006" className="hover:text-gray-400">Yashdeep Singh</a>
           <a href="https://github.com/om-Kamboj1409" className="hover:text-gray-400">Om Kamboj</a>
           <a href="https://github.com/QuantumG0D" className="hover:text-gray-400">Rakshit Sharma</a>
           <a href="https://github.com/Tarshit-Gupta" className="hover:text-gray-400">Tarshit Gupta</a>
           <a href="https://github.com/rehankhanchi" className="hover:text-gray-400">Rehan Khanchi</a>
+        </div>
+        <div className="mt-2">
+          <a href="direct/contact" className="hover:text-gray-400">Contact Us</a>
         </div>
       </div>
     </footer>
